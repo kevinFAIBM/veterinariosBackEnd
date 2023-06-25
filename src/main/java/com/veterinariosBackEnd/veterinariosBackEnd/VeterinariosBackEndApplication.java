0e687f9a6a69482b982646b8bd90dcddc762b7a5
kevinFAIBM <131728292+kevinFAIBM@users.noreply.github.com>
2023-06-24 18:01:46 -0600

    first commit

---
+++ src/main/java/com/veterinariosBackEnd/veterinariosBackEnd/VeterinariosBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.veterinariosBackEnd.veterinariosBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariosBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veterinariosBackEnd/veterinariosBackEnd/VeterinariosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinariosBackEnd.veterinariosBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariosBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
